09c5f235de736d3a524d4a3465fd851f2b277de5
Liu wangshu <525010411@qq.com>
2017-12-25 18:37:15 +0800

    添加新的冒泡算法

--- sort/primarysort/src/main/java/com/example/BubbleSort2.java
+++ sort/primarysort/src/main/java/com/example/BubbleSort2.java
@@ -1,13 +1,13 @@
 package com.example;
 
 /**
- * Created by Administrator on 2017/2/17 0017.
+ * Created by Administrator on 2017/12/25 0025.
  */
 
 /**
- * 冒泡排序方式1
+ * 冒泡排序方式2
  */
-public class BubbleSort {
+public class BubbleSort2 {
     public static void main(String[] args) {
         int a[] = {5, 3, 2, 4, 1};
         ArrayUtils.printArray(a);
@@ -19,11 +19,11 @@ public class BubbleSort {
         int i, j, v;
         int n = a.length;
         for (i = 0; i < n - 1; i++) {
-            for (j = n - 1; j > i; j--) {
-                if (a[j] < a[j - 1]) {
+            for (j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
                     v = a[j];
-                    a[j] = a[j - 1];
-                    a[j - 1] = v;
+                    a[j] = a[j + 1];
+                    a[j + 1] = v;
                 }
             }
         }
